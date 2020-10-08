9a3f7bd6adce1aeb1bf2bc565f7ce24e2b392798
ReedBostic <Bosticreed@gmail.com>
2020-10-07 20:36:46 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Author: Reed Bostic
+#Date: 10/7/2020
+#Description: A program for converting celsius to fahrenheit.
+
+print("Please enter a Celsius temperature.")
+temp1 = float(input())
+print("The equivalent Fahrenheit temperature is", temp1 * 9/5 + 32)
